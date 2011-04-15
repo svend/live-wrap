795dff7687fc1e73880cb514c9ae03cc96d0c3b4
Svend Sorensen <svend@ciffer.net>
2011-04-15 13:00:38 -0700

    Add imbeded newline

--- config/chroot_local-hooks/avahi-enable_reflector.sh
+++ config/chroot_local-hooks/avahi-enable_reflector.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Enable avahi reflector mode
 
-sed -i '/\<enable-reflector\>/c\enable-reflector=yes' /etc/avahi/avahi-daemon.conf
+sed -i '/\<enable-reflector\>/c\
+ennable-reflector=yes' /etc/avahi/avahi-daemon.conf
